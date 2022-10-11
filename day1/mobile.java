53877715a2192fefb0d32182db483927c3d8193b
Kvelu7923 <115493064+Kvelu7923@users.noreply.github.com>
2022-10-11 10:04:22 +0530

    Add files via upload
    day 1

---
+++ day1/mobile.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class mobile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    String mobileBrandName="oppo f12 ";
+    System.out.println(mobileBrandName);
+    char mobileLogo =  'O' ;
+    System.out.println(mobileLogo);
+    short noOfMobilePiece = 20;
+    System.out.println(noOfMobilePiece);
+    int modelNumber = 91733;
+    System.out.println(modelNumber);
+    long mobileImeiNumber = 6557467564526532L;
+    System.out.println(mobileImeiNumber);
+    float mobilePrice = 28999.99F;
+    System.out.println(mobilePrice);
+    boolean isDamaged = false;
+    System.out.println(isDamaged);
+   
+	}
+
+}
