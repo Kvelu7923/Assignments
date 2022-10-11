97cfa4d8921240636049f8d8411dfb502516d829
Kvelu7923 <115493064+Kvelu7923@users.noreply.github.com>
2022-10-11 09:52:34 +0530

    Add files via upload
    day 1

---
+++ day1/TwoWheeler.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class TwoWheeler {
+  
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   int noOfWheels = 2;
+		   System.out.println("noOfWheels ="+noOfWheels);
+	       short noOfMirrors = 2;
+	       System.out.println("noOfMirrors ="+noOfMirrors);
+	       long chasisNumber = 242453536773356L;
+	       System.out.println("chasisNumber ="+chasisNumber);
+	       boolean isPunctured= false;
+	       System.out.println("isPunctured ="+false);
+	       String  bikeName = "Rx 100" ;
+	       System.out.println("bikeName ="+bikeName);
+	       double runningKilometer = 7676.1212;
+	       System.out.println("runningKilometer ="+runningKilometer); 
+	} 
+	}
